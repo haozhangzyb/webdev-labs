00f61ead92760166f3946688cd4799d2ad9d15cf
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2022-02-03 14:22:20 -0600

    finish lab05

--- lab05/02-theme-switcher/index.js
+++ lab05/02-theme-switcher/index.js
@@ -5,3 +5,26 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+
+const changeToDefault = () => {
+  document.querySelector("body").classList = "";
+};
+
+const changeToDesert = () => {
+  document.querySelector("body").classList = "desert";
+};
+
+const changeToOcean = () => {
+  document.querySelector("body").classList = "ocean";
+};
+
+const changeToHighContrast = () => {
+  document.querySelector("body").classList = "high-contrast";
+};
+
+document.getElementById("default").addEventListener("click", changeToDefault);
+document.getElementById("desert").addEventListener("click", changeToDesert);
+document.getElementById("ocean").addEventListener("click", changeToOcean);
+document
+  .getElementById("high-contrast")
+  .addEventListener("click", changeToHighContrast);
--- lab05/03-dyslexia-mode/index.js
+++ lab05/03-dyslexia-mode/index.js
@@ -2,3 +2,16 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+
+const changeToDyslexia = () => {
+  var current = document.querySelector("body").classList.value;
+  if (current == "dyslexia-mode") {
+    document.querySelector("body").classList = "";
+  } else {
+    document.querySelector("body").classList = "dyslexia-mode";
+  }
+};
+
+document
+  .getElementById("dyslexia-toggle")
+  .addEventListener("click", changeToDyslexia);
